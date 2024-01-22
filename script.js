7e7c515d4869fbe30927b66aef9bc4d6fc6e5c6d
Ulia <yliia192@gmail.com>
2024-01-22 10:58:00 +0300

    burger menu

--- script.js
+++ script.js
@@ -1,19 +1,20 @@
-() => {
-  window.onload = () => {
-    let burger = document.querySelector(".burger");
-    let menu = document.querySelector(".header");
-    let body = document.querySelector("body");
-    burger.addEventListener("click", (e) => {
-      burger.classList.toggle("active");
-      menu.classList.toggle("active");
-      body.classList.toggle("lock");
-    });
+;(() => {
+	window.onload = () => {
+		let burger = document.querySelector('.burger')
+		let menu = document.querySelector('.header-list')
+		let body = document.querySelector('body')
 
-    let list = document.ruerySelector(".header-container");
-    list.addEventListener("click", (e) => {
-      burger.classList.toggle("active");
-      menu.classList.toggle("active");
-      body.classList.toggle("lock");
-    });
-  };
-};
+		burger.addEventListener('click', e => {
+			burger.classList.toggle('active')
+			menu.classList.toggle('active')
+			body.classList.toggle('lock')
+		})
+
+		let list = document.querySelector('.header-container')
+		list.addEventListener('click', e => {
+			burger.classList.toggle('active')
+			menu.classList.toggle('active')
+			body.classList.toggle('lock')
+		})
+	}
+})()
